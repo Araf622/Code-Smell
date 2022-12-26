fa18fa92e358efc5c39cfe168652056b539eeba7
Yasir Arafat <76701945+Araf622@users.noreply.github.com>
2022-12-27 03:32:02 +0600

    Add files via upload

---
+++ java/src/main/java/workshop/FizzBuzz.java
@@ -0,0 +1,22 @@
+package workshop;
+
+/**
+ * Requirements:
+ * For factor of three print Fizz instead of the number
+ * For factor of five print Buzz instead of the number
+ * For numbers which are factors of both three and five print FizzBuzz instead of the number
+ */
+public class FizzBuzz {
+    public static String checkFactors(int number) {
+
+        if (number % 15 == 0) {
+            return "FizzBuzz";
+        }
+        else if (number % 3 == 0) return "Fizz";
+
+        else if (number % 5 == 0) return "Buzz";
+
+
+        return String.valueOf(number);
+    }
+}
